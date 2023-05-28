a1457299885c1824e76894dab5a117244a66f4bd
Amaury Chronowski <a.chronowski@hotmail.fr>
2023-05-28 15:41:57 +0200

    fin Main (encore des erreur mais peux pas tester)

--- Main.py
+++ Main.py
@@ -8,125 +8,48 @@ author : Amaury CHRONOWSKI
 from rplidar import RPLidar
 from serial import Serial
 import numpy as np
+import time
 import math
 import os
-import time
-import multiprocessing as mp
+
+
 
 # Bloque de code pour le mode télécommandé
 
-def telecomander(ser_ordi, ser_STM, obstacle, process_obstacle):
+def telecommande(ser_ordi, ser_STM):
+    """
+    Fonction qui permet de dessin de facon télécommander avec le robot.
+
+    Args:
+        ser_ordi(Serial object): Liaison série entre l'ordinateur de commande et l'ordinateur distant.
+        ser_STM(Serial object): Liaison série entre l'ordinateur de commande et la carte d'execution.
+    """
 
     boucle = True
-    ser_ordi.write(b'OK<')
     while(boucle):
-        obstacle.acquire()
+
+        # Réception de données et traitement
         command = ser_ordi.read_until(b"<")
-        if command == "fin<":
-            process_obstacle.kill()
+        command = str(command)
+        command = command[2:-1]
+
+        if command == "fin<": # Fin du programme
             boucle = False
-        if command == "z<":
+        if command == "z<": # Avancer 
             envoiInstruction("z", ser_STM)
-        if command == "q<":
+        if command == "q<": # Tourner à gauche
             envoiInstruction("q", ser_STM)
-        if command == "s<":
+        if command == "s<": # Reculer
             envoiInstruction("s", ser_STM)
-        if command == "d<":
+        if command == "d<": # Tourner à droite
             envoiInstruction("d", ser_STM)
+        if command == "a<": # Baisser le crayon
+            envoiInstruction("a", ser_STM)
+        if command == "e<": # Lever le crayon
+            envoiInstruction("e", ser_STM)
 
     envoiInstruction("f", ser_STM)
     print('Arrêt du programme de télécommande.')
-
-def detectionObstacle(ser_STM, lidar, obstacle):
-    """
-    Fonction de détéction et d'ésquive d'obstacle pour base roulante équipé de Lidar.
-
-    Args:
-        ser(Serial object): Liaison série entre l'ordinateur de commande et la carte d'execution.
-    """
-
-    #Variables
-    tour = [] # Stock les données d'un tour du Lidar
-    res = 0 # Stock le résultat de la fonction de traitement des données
-
-    try:
-        print('Début du programme de détection.... Appuyer sur Crl+C pour arréter.')
-
-        for mesure in lidar.iter_scans(): # Recupération des données
-          for scan in mesure: # Lecture de chaque mesure
-                angle = scan[1]
-                distance = scan[2]
-
-                if np.floor(angle)<=2: # Envoi des données au traitement et remise du tour à zéro
-                    res = traitement(tour, res, ser_STM, obstacle)    
-                    tour = []
-
-                if distance<=300 and (angle<90 or angle>270): # Selection des mesures selon les critaire de détection (en face du robot et a moins de 30 cm du Lidar)
-                    tour.append((angle, distance))
-                    
-    except KeyboardInterrupt: # Arrêt du programme
-        envoiInstruction("f", ser_STM)
-        print('Arrêt du programme de détection.')
-        
-def traitement(tour, not_move, ser, obstacle):
-    """
-    Fonction de traitement des mesures d'un tour du Lidar.
-
-    Args:
-        tour(list of tuples): Liste des mesures d'un tour, les données sont sous formme de tuples (angle, distance).
-        not_move(int): Variables qui comptabilise le nombre de fois qu'il n'y a pas eu d'instruction de mouvement.
-        ser(Serial object): Liaison série entre l'ordinateur de commande et la carte d'execution.
-    
-    Return:
-        not_move(int): Variables qui comptabilise le nombre de fois qu'il n'y a pas eu d'instruction de mouvement.
-    """
-
-    # Varibles indiquant la presence d'obstacle
-    gauche = False
-    droite = False
-
-    for scan in tour: # Lecture des données
-        distance = scan[1]
-        angle = scan[0]
-
-        if  abs(distance*math.sin(np.deg2rad(angle)))<100: # Obstacle considéré que l'osqu'il se situe sur le trajet de la base roulante.
-            if angle<90 :
-                droite = True
-                                
-            if angle-360>-90:
-                gauche = True
-
-    if gauche and droite: # Si l'obstacle se trouve devant la base roulante il tourne à gauche
-        try:
-            obstacle.acquire()
-        except:
-            pass
-        envoiInstruction("q", ser)
-        return 0
-    
-    if droite and not gauche: # Si l'obstacle se trouve à droite de la base roulante il tourne à gauche
-        try:
-            obstacle.acquire()
-        except:
-            pass
-        envoiInstruction("q", ser)
-        return 0
-    
-    if not droite and gauche: # Si l'obstacle se trouve à gauche de la base roulante il tourne à droite
-        try:
-            obstacle.acquire()
-        except:
-            pass
-        envoiInstruction("d", ser)
-        return 0
-    
-    if not gauche and not droite and not_move == 4: # S'il n'y a pas d'obstacle apres 4 passage dans la fonction on indique au robot de se déplacer tout droit(a changer)
-        if obstacle.get_value()==0:
-            obstacle.release()
-        return 0
-    
-    elif not gauche and not droite and not_move<4: # Compte de passage dans la fonction sans instruction de déplacement
-        return not_move+1
 
 def envoiInstruction(direction, ser):
     """
@@ -146,118 +69,212 @@ def envoiInstruction(direction, ser):
         ser.write(b'd00000')
     if direction=='f':
         ser.write(b'f00000')
+    if direction=='a':
+        ser.write(b'p00000')
+    if direction=='e':
+        ser.write(b'u00000')
+
+
 
 # Bloque de code pour le dessin
 
-def dessin(ser_STM, ser_ordi,lidar):
+def dessin(ser_STM, ser_ordi, lidar):
+    """
+    Fonction qui réalise un dessin a partir de données reçus.
+
+    Args:
+        ser_ordi(Serial object): Liaison série entre l'ordinateur de commande et l'ordinateur distant.
+        ser_STM(Serial object): Liaison série entre l'ordinateur de commande et la carte d'execution.
+        lidar(RPLidar object): Liaison avec le capteur Lidar qui nous donne la posision d'obstacle.
+    """
+
     boucle = True
     while(boucle):
-        ser_ordi.write("OK<")
+
+        ser_ordi.write("OK<") # Annonce d'attente de données
+
+        # Réception de données et traitement
         donnees = ser_STM.read_until(b"<")
-        if donnees != "stop<":
+        donnees = str(donnees)
+        donnees = donnees[2:-1]
+
+        if donnees[0] == "I": # TRaitement et utilisation des données reçus
             angles, distances = decodage(donnees)
             deplacement(ser_STM, ser_ordi, lidar, angles, distances)
-        else:
+
+        elif donnees == "stop<": # Fin du programme
             boucle = False
 
-
+    envoiInstruction("f", ser_STM)
+    print('Arrêt du programme de dessin.')
     
-
-
 def deplacement(ser_STM, ser_ordi, lidar, angles, distances):
-    msgra = "an0080"
-    msgrr = "rn0080"
+    """
+    Fonction qui réalise le dessin par étapes: 1) Rotaition, 2) Recule de la distance entre le centre des roue et le crayon, 3) Baisse le crayon,
+    4) Avance de la distance voulue, 5) Leve le crayon, 6) Avance de la distance entre le centre des roue et le crayon.
 
-    for i in range(len(angles)):
+    Args:
+        ser_ordi(Serial object): Liaison série entre l'ordinateur de commande et l'ordinateur distant.
+        ser_STM(Serial object): Liaison série entre l'ordinateur de commande et la carte d'execution.
+        lidar(RPLidar object): Liaison avec le capteur Lidar qui nous donne la posision d'obstacle.
+        angles(tuple): Enssemble des angles à réaliser pour le dessin.
+        distances(tuple): Enssemble des distance à parcourir pour le dessin.
+    """
+
+    # Distance entre le centre des roue et le crayon
+    msgra = "in0110" # Avancer
+    msgrr = "hn0110" #Reculer
+
+    for i in range(len(angles)): # Parcoure l'ensemble des points du dessin
         k = 0
-        for j in range(6):
-            match k:
-                case 0:
-                    erreur = envoieMsgDessin(msgrr, ser_STM, ser_ordi)
-                case 1:
-                    ser_STM.write(b"cD0000")
-                    time.sleep(2)
-                    erreur = ""
-                case 2:
-                    obstacle = True
-                    while(obstacle):
-                        ret = detectionObstacleDessin(lidar, distances[i])
-                        if ret == 0:
-                            obstacle = False
-                        else:
-                            ser_ordi.write(b"erreur obstacle<")
-                            ser_ordi.read_until(b"<")
-                    msg = "a" + distances[i]
-                    erreur = envoieMsgDessin(msg, ser_STM, ser_ordi)
-                case 3:
-                    ser_STM.write(b"cU0000")
-                    time.sleep(2)
-                    erreur = ""
-                case 4:
-                    erreur = envoieMsgDessin(msgra, ser_STM, ser_ordi)
-                case 5:
-                    msg = "t" + angles[i]
-                    erreur = envoieMsgDessin(msgra, ser_STM, ser_ordi)
-            if erreur ==  "":
+        for j in range(6): # Réalisation de toute les étapes pour chaque ségement
+
+            if k == 0: # Rotaition
+                msg = "t" + angles[i]
+                erreur = envoieMsgDessin(msg, ser_STM, ser_ordi)
+
+            elif k == 1: # Recule de la distance entre le centre des roue et le crayon
+                erreur = envoieMsgDessin(msgrr, ser_STM, ser_ordi)
+
+            elif k == 2: # Baisse le crayon
+                ser_STM.write(b'p00000')
+                time.sleep(2)
+                erreur = ""
+
+            elif k == 3: # Avance de la distance voulue
+                # La detection d'obstacle ne fonctionne pas, pas eu le temps de debugger
+                """
+                obstacle = True
+                while(obstacle):
+                    ret = detectionObstacleDessin(lidar, distances[i])
+                    if not ret:
+                        obstacle = False
+                    else:
+                        ser_ordi.write(b"erreur obstacle<")
+                        ser_ordi.read_until(b"<")
+                """
+                msg = "i" + distances[i]
+                erreur = envoieMsgDessin(msg, ser_STM, ser_ordi)
+
+            elif k == 4: # Leve le crayon
+                ser_STM.write(b'u00000')
+                time.sleep(2)
+                erreur = ""
+                
+            elif k == 5: # Avance de la distance entre le centre des roue et le crayon
+                erreur = envoieMsgDessin(msgra, ser_STM, ser_ordi)
+
+            
+            if erreur ==  "": # Pas d'erreur, étape suivante
                 k+=1
-            else:
-                ser_ordi.write(b"erreur a l'etape %i<", k)
+
+            else: # Communication des erreur à l'ordinateur distant pour resoudre l'erreur et recommancer l'action automatiquement
+                msg = "Erreur a l'etape " + str(k) + "<" 
+                msg = msg.encode()
+                ser_ordi.write(msg)
                 ser_ordi.read_until(b"<")
 
-def detectionObstacleDessin(lidar,distance_test):
-    for mesure in lidar.iter_scans(): # Recupération des données
+def detectionObstacleDessin(lidar, distance_test):
+    """
+    Fonction qui permet de détecter les obstacles sur le trajet du robot.
+
+    Args:
+        lidar(RPLidar object): Liaison avec le capteur Lidar qui nous donne la posision d'obstacle.
+        distance_test(str): Distance que doit parcourir le robot lors d'une étape.
+
+    Returns:
+        res(boolan): Indique s'il y a un obstacle (True) ou non (False) 
+    """
+    # Mise à l'echelle du dessin
+    if distance_test[0] == "n": # Milimètre
+        ordre = 1
+    if distance_test[0] == "c": # Centimètre
+        ordre = 10
+    if distance_test[0] == "m": # Mètre
+        ordre = 1000
+    distance_test = int(distance_test) * ordre
+
+    for mesure in lidar.iter_scans(): # Recupération des données du Lidar
         if len(mesure)!=0:
             for scan in mesure: # Lecture de chaque mesure
                 angle = scan[1]
                 distance = scan[2]
 
                 if np.floor(angle)<=2: # Envoi des données au traitement et remise du tour à zéro
-                    res = traitementdessin(tour,distance_test)    
+                    res = traitementdessin(tour)    
                     tour = []
+                    break
 
-                if distance<=300 and (angle<90 or angle>270): # Selection des mesures selon les critaire de détection (en face du robot et a moins de 30 cm du Lidar)
+                if distance <= distance_test and (angle >= 90 and angle <= 290): # Sélection des mesures selon les critaires de détections
                     tour.append((angle, distance))
-            break
+        break
     return res
 
-def  traitementdessin(tour,distance):
+def  traitementdessin(tour):
+    """
+    Fonction qui permet de détécter les obstacle dans les donnée retenus du Lidar.
+
+    Args:
+        tour(tuple): Ensemble des données du Lidar sur un tour.
+
+    Returns:
+        obstacle(boolan): Indique s'il y a un obstacle sur la route du robot.
+    """
     # Varibles indiquant la presence d'obstacle
-    gauche = False
-    droite = False
+    obstacle = False
 
     for scan in tour: # Lecture des données
         distance = scan[1]
         angle = scan[0]
 
-        if  abs(distance*math.sin(np.deg2rad(angle)))<distance+20: # Obstacle considéré que l'osqu'il se situe sur le trajet de la base roulante.
-            if angle<90 :
-                droite = True
-                                
-            if angle-360>-90:
-                gauche = True
+        if  (distance*math.sin(np.deg2rad(angle)) >= -200 and angle >= 180) or (distance*math.sin(np.deg2rad(angle)) <= 80 and angle <= 180): # Obstacle considéré que losqu'il se situe sur le trajet de la base roulante.
+            obstacle = True
 
-    if gauche or droite: # Si l'obstacle se trouve devant la base roulante il tourne à gauche
-        return 1
-    
-    if not gauche and not droite: 
-        return 0
-
-
-
-
-
+    return obstacle
 
 def envoieMsgDessin(msg, ser_STM, ser_ordi):
-    ser_STM.write(b"%s",msg)
+    """
+    Fonction d'envoie d'instruction au robot.
+
+    Args:
+        msg(str): Message a envoyé.
+        ser_ordi(Serial object): Liaison série entre l'ordinateur de commande et l'ordinateur distant.
+        ser_STM(Serial object): Liaison série entre l'ordinateur de commande et la carte d'execution.
+    
+    Returns:
+        erreur: Indique s'il y a une erreur
+    """
+    
+    msg_temp = msg
+
+    # Envoie de la commande à la STM32
+    msg = msg.encode()
+    ser_STM.write(msg)
+
+    # Reception de réponce de la STM32 en cas d'erreur
     ret = ser_STM.read_until(b"K")
     if ret == "NACK":
-        erreur = msg[:2]
-        ser_ordi.write(b"erreur robot<")
+        erreur = msg
     else:
         erreur = ""
+        if msg_temp[1] == "+" or msg_temp[1] == "-": # Laisse le temps à la rotation de se faire
+            ang = int(msg_temp[2]) + (int(msg_temp[3:]) / 1000)
+            temps = ang/0.107 # 0.107 rad/s (vitesse de rotation)
+            time.sleep(temps)
     return erreur
 
 def decodage(donnees):
+    """
+    Décode le chemin du dessin reçu.
+
+    Args:
+        donnees(str): Données reçus à décoder
+
+    Returns:
+        aangles(tuple): Enssemble des angles à réaliser pour le dessin.
+        distances(tuple): Enssemble des distance à parcourir pour le dessin.
+    """
+
     # Initialisation des listes angle et distance
     angle = []
     distance = []
@@ -269,44 +286,51 @@ def decodage(donnees):
     for substring in substrings:
         if substring.startswith("I+"):
             angle.append(substring[1:6])
-            distance.append(substring[7:11])
+            distance.append(substring[7:12])
         elif substring.startswith("+"):
             angle.append(substring[0:5])
-            distance.append(substring[6:10])
+            distance.append(substring[6:11])
         elif substring.startswith("-"):
             angle.append(substring[0:5])
-            distance.append(substring[7:11])
+            distance.append(substring[7:12])
     
     # Retourne les listes d'angles et de distances
     return angle, distance
 
+
+
+# Fonction principal
+
 def main():
+    """
+    Fonction principal qui permet de choisir dans quel mode on va se mettre.
+    """
+
     # Connection
     ser_STM = Serial(port="dev/ttyACM0", baudrate=19200)
-    ser_ordi= Serial(port="COM5", baudrate=19200)
+    ser_ordi= Serial(port="dev/ttyUSB1", baudrate=19200)
     lidar = RPLidar('dev/ttyUSB0')
 
-    # Premiere communication avec l'ordinateur
-    ser_ordi.write(b'OK<') # Indique que la communication peut etre commencer
-    instruction = ser_ordi.read_until('<')
+    boucle = True
+    while(boucle):
 
-    
-    if instruction == "telecomander<":
-        obstacle = mp.Semaphore(1)
-        process_obstacle = mp.Process(target = detectionObstacle, args = (ser_STM, lidar, obstacle,))
-        process_obstacle.start()
-        process_obstacle.join()
+        # Réception de données et traitement
+        instruction = ser_ordi.read_until('<')
+        instruction = str(instruction)
+        instruction = instruction[2:-1]
 
-        process_telecomander = mp.Process(target = telecomander, args=(ser_ordi, ser_STM, obstacle, process_obstacle,))
-        process_telecomander.start()
-        process_telecomander.join()
+        # Séléction du mode
+        if instruction == "telecommande<":
+            telecommande(ser_ordi, ser_STM)
 
-    if instruction == "dessin<":
-        process_dessin = mp.Process(target = dessin, args = (ser_STM, ser_ordi, lidar,))
-        
-        process_dessin.start()
-        process_dessin.join()
+        if instruction == "dessin<":
+            dessin(ser_STM, ser_ordi, lidar)
 
+        if instruction == "stop<":
+            boucle = False
+
+    # Arrêt du robot
+    print("Arrêt du robot")
     envoiInstruction("f", ser_STM)
     lidar.stop()
     lidar.disconnect()
